a3cd72e42830fb9d1ba42b9ca4b784f4dbe392d5
pratiksha <pratiksha.r1818@gmail.com>
2021-10-19 15:08:12 +0530

    commit

---
+++ com.crm.maven/src/test/java/crm/Maven/Project1/CreateContactTest.java
@@ -0,0 +1,19 @@
+package crm.Maven.Project1;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+ @Test
+	public void CreateContactTest() {
+		//access parameters from commondline(profiling)
+		//String URL = System.getProperty("url");
+		//System.out.println(URL);
+		System.out.println("newsmokes1");
+	}
+  @Test
+	public void EditContactTest() {
+		System.out.println("newsmokes2");
+	}
+	
+
+}
---
+++ com.crm.maven/src/test/java/crm/Maven/Project1/CreateOrgTest.java
@@ -0,0 +1,16 @@
+package crm.Maven.Project1;
+
+import org.testng.annotations.Test;
+
+
+public class CreateOrgTest {
+	@Test
+	public void CreateOrgTest() {
+		System.out.println( "newmaven" );
+	}
+	@Test
+	public void EditOrgTest() {
+		System.out.println( "nwemaventest" );
+	}
+
+}
